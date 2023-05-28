46ef4b92743c4453c5bb16813666699ea434519c
adriano <adrianoromu@gmail.com>
2023-05-28 17:09:22 -0300

    tela de Login

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -19,7 +19,7 @@ export default function Example() {
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
+                Email addresssss
               </label>
               <div className="mt-2">
                 <input
